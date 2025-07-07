216bfa3a77a18d4764276622d8c28ddcaf40bd8c
zq <2523589960@qq.com>
2025-07-07 16:45:55 +0800

    修复github action配置；

--- apps/example/vite.config.ts
+++ apps/example/vite.config.ts
@@ -4,24 +4,20 @@ import cesium from 'vite-plugin-cesium'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  const basePath = process.env.VITE_BASE_PATH || '/'
-  return {
-    base: basePath,
-    plugins: [vue(), cesium()],
-    resolve: {
-      // https://cn.vitejs.dev/config/#resolve-alias
-      alias: {
-        // 设置路径
-        '~': path.resolve(__dirname, './'),
-        // 设置别名
-        '@': path.resolve(__dirname, './src'),
-      },
-      // https://cn.vitejs.dev/config/#resolve-extensions
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+export default defineConfig({
+  plugins: [vue(), cesium()],
+  resolve: {
+    // https://cn.vitejs.dev/config/#resolve-alias
+    alias: {
+      // 设置路径
+      '~': path.resolve(__dirname, './'),
+      // 设置别名
+      '@': path.resolve(__dirname, './src'),
     },
-    server: {
-      host: true,
-    },
-  }
+    // https://cn.vitejs.dev/config/#resolve-extensions
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+  },
+  server: {
+    host: true,
+  },
 })
